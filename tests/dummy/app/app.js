ed9c4361f971fddc494b8682c5696e558bdaa6f3
Jeff Jewiss <jeffjewiss@gmail.com>
2017-08-10 16:21:21 +0100

    Fix lint issues

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,18 +1,18 @@
-import Ember from 'ember';
-import Resolver from './resolver';
-import loadInitializers from 'ember-load-initializers';
-import config from './config/environment';
+import Ember from 'ember'
+import Resolver from './resolver'
+import loadInitializers from 'ember-load-initializers'
+import config from './config/environment'
 
-let App;
+const { Application } = Ember
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
+let App
 
-App = Ember.Application.extend({
+App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
-});
+})
 
-loadInitializers(App, config.modulePrefix);
+loadInitializers(App, config.modulePrefix)
 
-export default App;
+export default App
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,15 +1,19 @@
-import Ember from 'ember';
-import Application from '../../app';
-import config from '../../config/environment';
+import Application from '../../app'
+import config from '../../config/environment'
+import { run } from 'ember-runloop'
+import { assign } from 'ember-platform'
 
-export default function startApp(attrs) {
-  let attributes = Ember.merge({}, config.APP);
-  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+export default function startApp (attrs) {
+  let application
 
-  return Ember.run(() => {
-    let application = Application.create(attributes);
-    application.setupForTesting();
-    application.injectTestHelpers();
-    return application;
-  });
+  let attributes = assign({}, config.APP)
+  attributes = assign(attributes, attrs) // use defaults, but you can override;
+
+  run(() => {
+    application = Application.create(attributes)
+    application.setupForTesting()
+    application.injectTestHelpers()
+  })
+
+  return application
 }
