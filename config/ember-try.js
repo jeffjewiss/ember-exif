98a2982655f06afe725fe5beaa9006105b45188a
Jeff Jewiss <jeff@jeffjewiss.com>
2018-01-18 17:08:02 +0000

    Stop testing beta and canary for now

--- config/ember-try.js
+++ config/ember-try.js
@@ -48,38 +48,6 @@ module.exports = {
       }
     },
     {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
       name: 'ember-default',
       npm: {
         devDependencies: {}
